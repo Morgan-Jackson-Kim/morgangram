12c92bf92e84fd29a2444ce3fa5758f37fdf3e6c
DESKTOP-JFG7H38\MorganKim <kjy2696@gmail.com>
2021-08-17 11:18:30 +0900

    first import

---
+++ src/main/java/com/morganj/morgandram/MorgandramApplication.java
@@ -0,0 +1,13 @@
+package com.morganj.morgandram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorgandramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorgandramApplication.class, args);
+	}
+
+}
